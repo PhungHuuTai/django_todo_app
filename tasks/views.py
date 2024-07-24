a69be4a274850efb0d099ea85372cbe9baf774c5
PhungHuuTai <phunghuutai7@gmail.com>
2024-07-24 23:47:11 +0700

    my third commit

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 
 # Create your views here.
@@ -24,10 +24,51 @@ def remaining(request):
     })
 
 def add_task(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        due_date = request.POST.get('due_date')
+        due_time = request.POST.get('due_time')
+        completed = False
+
+        if title != "" and due_date != "" and due_time !="":
+            task = Task(
+                title=title,
+                description=description,
+                due_date=due_date,
+                due_time=due_time,
+                completed=completed
+            )
+            task.save()
+            return redirect('home')
+    else:
+        return render(request, 'add_task.html') 
     return render(request, 'add_task.html')
 
-def delete(request):
-    return render(request, 'delete.html')
+def delete(request, task_id):
+    task = Task.objects.get(id=task_id)
 
-def task_detail(request):
-    return render(request, 'task_detail.html')
+    return render(request, 'delete.html', {
+        'task': task,
+    })
+
+def task_detail(request, task_id):
+    task = Task.objects.get(id=task_id)
+
+    return render(request, 'task_detail.html', {
+        "task": task,
+    })
+
+def toggle_complete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if task:
+        task.completed = not task.completed
+        task.save()
+
+        return redirect('home')
+    
+def remove_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if task:
+        task.delete()
+        return redirect('home')
